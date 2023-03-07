523b3ba401af323c5a501f46ecd77af80def0ab0
abdulbosit <abdulbosit19980204@gmail.com>
2023-03-07 16:43:56 +0500

    first commit

---
+++ learn.js
@@ -0,0 +1,23 @@
+let text = document.querySelector('h1'),
+    ptext = document.getElementsByTagName('h6'),
+    primaryBtn = document.getElementsByClassName('primary-btn'),
+    newsletterBtn = document.getElementsByClassName('newsletter-btn'),
+    myForm = document.getElementsByClassName('myForm'),
+    submit = document.querySelectorAll('submit')
+console.log(text, ptext, primaryBtn)
+
+
+text.addEventListener('mouseenter', function() {
+    text.textContent = 'O`zgardi'
+})
+text.addEventListener('mouseleave', function() {
+    text.textContent = 'Qayti'
+})
+
+primaryBtn.addEventListener('click', function() {
+    text.style.fontsize(100)
+})
+
+submit.addEventListener('click', function() {
+    text.styly.fontsize = '100'
+})
